e15646ca72072a634b7fe59368276506879a1a75
proggreg <greg.field1992@gmail.com>
2020-09-19 13:03:34 +0100

    changed email to use OAuth2

--- routes/email.js
+++ routes/email.js
@@ -3,22 +3,23 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require("nodemailer");
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
+
+// Nodemailer OAuth2 Config
+var smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    secure: true,
     auth: {
+        type: 'OAuth2',
         user: process.env.email,
-        pass: process.env.email_password
+        clientId: process.env.clientId,
+        clientSecret: process.env.clientSecret,
+        refreshToken: process.env.refreshToken,
+        accessToken: process.env.accessToken
     }
 });
 
 // Receive contact form and send email 
 router.post('/send', (req, res, next) => {
-
-    if (transporter.options.auth.user === undefined || transporter.options.auth.pass === undefined) {
-        res.status(404).json({
-            error: "No credentials found"
-        })
-    }
 
     let mailOptions = {
         from: req.body.from,
@@ -26,17 +27,19 @@ router.post('/send', (req, res, next) => {
         subject: req.body.subject,
         text: req.body.msg
     }
-    try {
-        transporter.sendMail(mailOptions, function (err, data) {
-            if (err) {
-                next(err);
-            } else {
-                res.send("Email sent successfully")
-            }
-        });
-    } catch (err) {
-        next(err)
-    }
+
+    smtpTransport.sendMail(mailOptions, function (err) {
+        // TODO better error handling may be needed
+        if (err) {
+            console.log({
+                err
+            });
+            res.status(404).json("Problem sending email");
+        } else {
+            res.send("Email sent successfully")
+        }
+        smtpTransport.close();
+    });
 });
 
 module.exports = router;
