2a7f9ebdad25b4706073700f326de55ea0bae073
proggreg <greg.field1992@gmail.com>
2020-09-22 12:06:06 +0100

    basic error handling for reading cv file

--- routes/file.js
+++ routes/file.js
@@ -3,13 +3,23 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 const fs = require('fs');
+const {
+    nextTick
+} = require('process');
 
-router.get('/CV', (req, res) => {
-    var cv = path.resolve('../proggreg-backend/assets/CV.pdf');
-    var binaryData = fs.readFileSync(cv);
-    var base64String = new Buffer.from(binaryData).toString("base64");
-
-    res.send(base64String);
+router.get('/CV', (req, res, next) => {
+    var cv = path.resolve('../proggreg-backend/asets/CV.pdf');
+    fs.readFile(cv, (err, data) => {
+        if (err) {
+            res.status(404).json({
+                errorMessage: "File not found"
+            })
+            next(err);
+        } else {
+            var base64String = new Buffer.from(data).toString("base64");
+            res.send(base64String);
+        }
+    });
 })
 
 module.exports = router;
